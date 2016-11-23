978b9ce3dc6719437bc5be42620c1ef2792e7e06
ddt.jissyusei1 <ddt.jissyusei1@gmail.com>
2016-11-23 21:30:11 +0900

    Revert "Revert "本日、会社作業分""
    This reverts commit f70424659653f2263669667dd7a73fa46cf9277a.

--- src/main/resources/templates/js/TopWindowDetail.js
+++ src/main/resources/templates/js/TopWindowDetail.js
@@ -14,4 +14,111 @@
  */
 function TopWindowDetail() {
 	
+	WindowDesign.call(this);	//スーパークラスのコンストラクタを呼ぶ
+	
+	/**
+	 * 関数名：	isCheckDateFromTo
+	 * 概要：		検索実行時のFromToの日付大小関係を検証する
+	 * 引数：		date:from	fromに入力された日付
+	 * 			date:to		toに入力された日付
+	 * 戻り値：	boolean		正しいならtrue、エラーならfalseを返す
+	 * 作成日：	2016/11/23
+	 * 作成者：	k.urabe
+	 */
+	this.isCheckDateFromTo = function(from, to) {
+		
+		var returnBoolean = false;		// 返却用の変数を宣言し、初期値としてfalseをセットする
+		
+		// from toの大小関係を検証する
+		if(from <= to) {
+			// 正しい関係にあるのでtrueをセットする
+			returnBoolean = true;
+		}
+		
+		// 判定した結果を返す
+		return returnBoolean;
+		
+	}
+	
+	/**
+	 * 関数名：	clickSearchButton
+	 * 概要：		Top画面の検索実行時や、初期表示処理、リロード時に実行
+	 * 引数：		なし
+	 * 戻り値：	なし
+	 * 作成日：	2016/11/23
+	 * 作成者：	k.urabe
+	 */
+	this.clickSearchButton = function() {
+		
+		var jsonArray = {};			// リクエストに使用するjson連想配列を作成する
+		
+		// 日報の描画領域をすべて削除する
+		$(KEY_DIV).remove(SELECTOR_PARENT_AREA);
+		
+		// 検索領域のfromを取得する
+		jsonArray[KEY_SERACH_FROM_DATE] = getWindowItem(SELECTOR_SERACH_FROM_DATE);
+		// 検索領域のtoを取得する
+		jsonArray[KEY_SERACH_TO_DATE] = getWindowItem(SELECTOR_SERACH_TO_DATE);
+		// 検索領域のユーザを取得する
+		jsonArray[KEY_SERACH_USER] = getWindowItem(SELECTOR_SERACH_USER);
+		// 検索領域の既読を取得する
+		jsonArray[KEY_SERACH_READ] = getWindowItem(SELECTOR_SERACH_READ);
+		// 検索領域の下書を取得する
+		jsonArray[KEY_SERACH_NOTE] = getWindowItem(SELECTOR_SERACH_NOTE);
+
+		// JSON連想配列を用いてDBから値を取得する
+		this.getJsonData(PATH_TOP, jsonArray, STR_READ);
+		// 取得したJSONを用いてHTMLへタグを展開する
+		this.createContentIndex(SELECTOR_REPORT_AREA, 0);
+		
+		// TODO:【未実装】ボタンの数だけステップが増えるので、別関数検討
+		// TODO:【メモ】ボタンの設置は画面初期表示（固定パーツ）と、タグ展開時（流動パーツ）で行う。ここではイベント登録のみ
+		// TODO:【メモ】↑ 現在はタグ展開時にボタンの配置までやっていない
+		// TODO:【メモ】同じタグの複数箇所に一気にイベント登録しようとしているが、これが可能か不明。場合によってはタグの展開時に1つ1つボタンの展開に合わせてイベント登録が必要かも
+		// 流動的なパーツのイベントを登録する
+		// 新規コメントボタンのイベントを登録する
+		this.setClickEvent(SELECTOR_B_NEW_COMMENT, prepareAnotherWindow);
+		// TODO:【メモ】アコーディオンと共通化するため、個別には必要ない？
+		// 閉じるボタンのイベントを登録する
+		//setClickEvent(SELECTOR_B_CLOSE, );
+		// 未読にするボタンのイベントを登録する
+		this.setClickEvent(SELECTOR_B_NO_READ, clickAddContentButton);
+		// いいねボタンのイベントを登録する
+		this.setClickEvent(SELECTOR_B_FAVORITE, clickAddContentButton);
+		// 編集ボタンのイベントを登録する
+		this.setClickEvent(SELECTOR_B_EDIT, prepareAnotherWindow);
+		// 削除ボタンのイベントを登録する
+		this.setClickEvent(SELECTOR_B_DELETE, clickDeleteButton);
+		// アコーディオン対象ボタンのイベントを登録する
+		this.managementAccordion(SELECTOR_B_ACCORDION, SELECTOR_ACCORDION_AREA);
+		
+	}
+	
+	/**
+	 * 関数名：	createTopWindow
+	 * 概要：		Top画面を開いた時の処理
+	 * 引数：		なし
+	 * 戻り値：	なし
+	 * 作成日：	2016/11/23
+	 * 作成者：	k.urabe
+	 */
+	this.createTopWindow = function() {
+		
+		// TODO:【未実装】logoutボタンはサーバ側のSpringで制御予定のため、特に記述なし。ただし、ダイアログを出さないといけないので、追加する必要あり
+		// 画面共通部分のボタンにイベントを登録する
+		// 検索ボタンのイベントを登録する
+		this.setClickEvent(SELECTOR_B_SERACH, clickSearchButton);
+		// 新規日報ボタンのイベントを登録する
+		this.setClickEvent(SELECTOR_B_NEW_REPORT, prepareAnotherWindow);
+		
+		// TODO:【メモ】サーバから取得したHTMLには、初期表示として値が入っていることを想定
+		// 初期の検索条件にて画面項目の取得を行う。
+		this.clickSearchButton();
+		
+	}
+	
 }
+
+TopWindowDetail.prototype = new WindowDesign();
+//サブクラスのコンストラクタを有効にする
+TopWindowDetail.prototype.constructor = WindowDesign;
--- src/main/resources/templates/js/WindowDesign.js
+++ src/main/resources/templates/js/WindowDesign.js
@@ -13,9 +13,10 @@
  * 作成者：	k.urabe
  */
 function WindowDesign() {
+	BaseWindow.call(this);	//スーパークラスのコンストラクタを呼ぶ
 	
-	this.json;		// 取得したjsonを格納
-	this.dom;		// 取得したTHMLを格納
+	this.json;				// 取得したjsonを格納
+	this.dom;				// 取得したTHMLを格納
 	
 	/**
 	 * 関数名：	createContentIndex
@@ -29,13 +30,54 @@ function WindowDesign() {
 	 * 作成者：	k.urabe
 	 */
 	// TODO:【メモ】設計ではJSONを引数で渡していたが、メンバとして保存されているので、thisで使用する
-	this.createContentIndex = function(selector, grand_parent_content_id, parent_content_id, indent = 0) {
+	// selectorの後に引数としてgrand_parent_content_idを追加検討
+	this.createContentIndex = function(selector, parent_content_id, indent = 0) {
+		
+		var jsonLen;		// jsonの長さを保持するための変数
 		
 		// jsonが取得出来ているか検証する
 		if(this.json !== null | this.json !== undefined | this.json[message]) {
 			
-			// セレクタに応じたテーブルの開始タグを取得して追加する
-			$(selector).append(this.getIndexTag(selector));
+			// indentで1階層目か判定する
+			if(indent == 0) {
+				// 1階層目なのでセレクタに応じたテーブルの開始タグを取得して追加する
+				$(selector).append(this.getIndexTag(selector));
+			}
+			// JSONの長さを取得
+			jsonLen = this.json.length;
+			
+			// TODO:【未実装】子要素の呼び出しについて、今のままでは適切でない。
+			// JSONの行要素を走査する
+			for(var key in this.json) {
+				// TODO:【未実装】レポートの概要を展開する際は、複数のコンテンツIDがJSONに含まれる。
+				// TODO:【未実装】それにも関わらず、受け取らなければならないので、ロジックが矛盾する
+				// TODO:【未実装】暫定として0を引数として受け取り、0の際は子を検証しない動きとする
+				// 対象レコードが子要素であるか検証する
+				if(parent_content_id < this.json[key].parent_content_id && parent_content_id !== 0) {
+					// 子要素を出力するため再帰呼び出しを行う。
+					this.createContentIndex(selector, this.json[key].parent_content_id, indent + 1)
+				}
+				// indent数分、ループしてテーブルのインデントをずらす
+				for(var i = 0; i < indent; i++) {
+					// indent用のタグを挿入する
+					$(selector).append(TAG_INDENT);
+				}
+				// 行開始のタグを挿入する
+				// TODO:【セレクタ】行の開始にも識別可能なクラス名など設けるか
+				$(selector).append(TAG_TR_START);
+				// JSONの列要素を走査する
+				for(var keyIn in key) {
+					// 項目タグと値をセットする
+					$(selector).append(TAG_TD_START + key[keyIn] + TAG_TD_END).addClass(keyIn);
+				}
+				// 行終了のタグを挿入する
+				$(selector).append(TAG_TR_END);
+			}
+			// indentで1階層目か判定する
+			if(indent == 0) {
+				// 1階層目なのでテーブルの終了タグを追加する
+				$(selector).append(TAG_TOP_TABLE_END);
+			}
 		
 		// JSONにメッセージがあれば
 		} else if(this.json[message]) {
@@ -43,4 +85,184 @@ function WindowDesign() {
 			$(selector > SELECTOR_SERACH_MESSAGE).text(this.json[message]);
 		}
 	}
+	
+	/**
+	 * 関数名：	getJsonData
+	 * 概要：		サーバに対してリクエストを送り、結果としてJSONを得る
+	 * 引数：		String:path		リクエスト先のURL
+	 * 			json			jsonの連想配列
+	 * 			String:type		リクエストの種類。サーバ側でマッピングに使用
+	 * 戻り値：	なし
+	 * 作成日：	2016/11/23
+	 * 作成者：	k.urabe
+	 */
+	this.getJsonData = function(path, json, type) {
+		
+		var map;		// json連想配列を文字列かしたものを格納するための変数
+		
+		// json連想配列から、JSON文字列に変換、取得する
+		map = JSON.stringify(json);
+		
+		// ajax通信を行う
+		$.ajax({
+			// リクエストURL
+			url: path,
+			// 取得ファイルの形式をJSONに指定する
+			dataType: 'JSON',
+			// postメソッドで通信する
+			type: 'POST',
+			// 同期通信を行う
+			async: false,
+			// サーバへ渡すデータを連想配列にする
+			data: {crud:type,json:map},
+			// キャッシュを無効にする
+			cache: false,
+			// 通信成功時の処理
+			success: function(json) {
+				// 取得したjsonをメンバへ格納する
+				this.json = json;
+			},
+			error: function(xhr, status, error) {
+				// TODO:【未実装】メッセージおよび例外処理について未実装
+				// 処理失敗の旨を出力する
+				alert("");
+			}
+		});
+		
+	}
+	
+	/**
+	 * 関数名：	getHtmlData
+	 * 概要：		サーバに対してリクエストを送り、結果としてHTMLを得る
+	 * 引数：		String:path		リクエスト先のURL
+	 * 戻り値：	なし
+	 * 作成日：	2016/11/23
+	 * 作成者：	k.urabe
+	 */
+	this.getHtmlData = function(path) {
+		
+		// ajax通信を行う
+		$.ajax({
+			// リクエストURL
+			url: path,
+			// 取得ファイルの形式をHTMLに指定する
+			dataType: 'HTML',
+			// postメソッドで通信する
+			type: 'POST',
+			// 同期通信を行う
+			async: false,
+			// キャッシュを無効にする
+			cache: false,
+			// 通信成功時の処理
+			success: function(html) {
+				// 取得したhtmlをメンバへ格納する
+				this.dom = html;
+			},
+			error: function(xhr, status, error) {
+				// TODO:【未実装】メッセージおよび例外処理について未実装
+				// 処理失敗の旨を出力する
+				alert("");
+			}
+		});
+		
+	}
+	
+	/**
+	 * 関数名：	createReportDetail
+	 * 概要：		日報の概要に対する内容を展開する
+	 * 引数：		selector		
+	 * 戻り値：	なし
+	 * 作成日：	2016/11/23
+	 * 作成者：	k.urabe
+	 */
+	this.createReportDetail = function(selector) {
+		
+		// json内にkey:messageがないか検証する（あれば本関数は何もしない）
+		if(!this.json.hasOwnProperty(STR_MESSAGE)) {
+			
+			// セレクタに応じたテーブルの開始タグを取得して追加する
+			$(selector).append(this.getIndexTag(selector));
+			
+			// JSONの行要素を走査する
+			for(var key in this.json) {
+				// 行開始のタグを挿入する
+				// TODO:【セレクタ】行の開始にも識別可能なクラス名など設けるか
+				$(selector).append(TAG_TR_START);
+				// JSONの列要素を走査する
+				for(var keyIn in key) {
+					// 項目タグと値をセットする
+					$(selector).append(TAG_TD_START + key[keyIn] + TAG_TD_END).addClass(keyIn);
+				}
+				// 行終了のタグを挿入する
+				$(selector).append(TAG_TR_END);
+			}
+			// テーブルの終了タグを追加する
+			$(selector).append(TAG_TOP_TABLE_END);			
+		}
+		
+	}
+	
+	/**
+	 * 関数名：	createCommentDetail
+	 * 概要：		コメント概要に紐付く詳細（最初の数行）を展開する
+	 * 引数：		selector		
+	 * 戻り値：	なし
+	 * 作成日：	2016/11/23
+	 * 作成者：	k.urabe
+	 */
+	this.createCommentDetail = function(selector) {
+		
+		// HTMLの対象エリア内を走査する
+		// TODO:【セレクタ】コメント概要を囲んでいるエリアがセレクタで渡される想定(blockArea?)
+		// TODO:【セレクタ】その配下の
+		$(selector).each(function(index) {
+			// JSON内の走査時にHTMLのセレクタを指定出来るよう変数へ待避する
+			thisElem = this;
+			
+			// JSON内を走査して、当該コメント概要に紐付く内容を探す
+			$.each(this.json, function(key, value) {
+				// 各行のコンテンツIDとJSON内のコンテンツIDが一致するか検証(一致ならば出力対象)
+				if($(thisElem > SELECTOR_CONTENT_ID).text() == this.json[key].KEY_CONTENT_ID) {
+					// 行の開始としてtrを挿入する
+					$(thisElem).append(TAG_TR_START);
+					// 項目の要素を出力する
+					$(thisElem).append(TAG_TD_START + this.json[key].KEY_MAIN_TEXT + TAG_TD_END).addClass(key);
+					// 行終了のタグを挿入する
+					$(thisElem).append(TAG_TR_END);
+				}
+			});
+		});
+		
+	}
+	
+	/**
+	 * 関数名：	getIndexTag
+	 * 概要：		JSON展開時、セレクタに応じた見出し行やテーブルの開始タグを返す
+	 * 引数：		selector		
+	 * 戻り値：	String returnTag
+	 * 作成日：	2016/11/23
+	 * 作成者：	k.urabe
+	 */
+	this.getIndexTag = function(selector) {
+		
+		var returnTag;			// 返却用のtagを格納するための変数
+		
+		// TODO:【未実装】今後、セレクタによってタグを振り分けるために使用。
+		// セレクタによって返すタグを振り分ける
+		switch(selector) {
+			case "dummy":
+				break;
+			default:
+				// TODO:【セレクタ】デフォルトで返すものも、全体のタグ構成が見えてきたら検証し直し。
+				// デフォルトとしてトップ画面のテーブルタグを返す
+				returnTag = TAG_TOP_TABLE_START;
+		}
+		// 決定したtagを返す
+		return returnTag;
+	}
+	
 }
+
+WindowDesign.prototype = new BaseWindow();
+//サブクラスのコンストラクタを有効にする
+WindowDesign.prototype.constructor = BaseWindow;
