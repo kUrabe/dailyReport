da4daa1a9f3599350388901a3a3a7b6f8c291810
ddt.jissyusei1 <ddt.jissyusei1@gmail.com>
2016-12-21 12:50:55 +0900

    2016/12/21 わるいね機能追加中 残：いいねとの排他関係（後回し）

--- src/main/resources/static/js/ChartSummaryWindowDetail.js
+++ src/main/resources/static/js/ChartSummaryWindowDetail.js
@@ -1,38 +1,34 @@
 /**
- * ファイル名：	UserListWindowDetail.js
- * 概要：		ユーザ一覧画面系の処理を集めたクラスを定義したファイル。
- * 作成日：	2016/12/14
+ * ファイル名：	ChartSummaryWindowDetail.js
+ * 概要：		グラフ集計画面系の処理を集めたクラスを定義したファイル。
+ * 作成日：	2016/12/21
  * 作成者：	k.urabe
- * path：		js/TopWindowDetail.js
+ * path：		js/ChartSummaryWindowDetail.js
  */
 
 /**
- * クラス名：	UserListWindowDetail
- * 概要：		ユーザ一覧画面系の処理を集めたクラス。WindowDesignクラスを継承する。
- * 作成日：	2016/12/14
+ * クラス名：	ChartSummaryWindowDetail
+ * 概要：		グラフ集計画面系の処理を集めたクラス。WindowDesignクラスを継承する。
+ * 作成日：	2016/12/21
  * 作成者：	k.urabe
  */
-function UserListWindowDetail() {
+function ChartSummaryWindowDetail() {
 	
 	WindowDesign.call(this);	//スーパークラスのコンストラクタを呼ぶ
 	
 	/**
 	 * 関数名：	createUserListWindow
-	 * 概要：		ユーザ一覧画面の初期表示機能
+	 * 概要：		画面の初期表示機能
 	 * 引数：		なし
 	 * 戻り値：	なし
-	 * 作成日：	2016/12/14
+	 * 作成日：	2016/12/21
 	 * 作成者：	k.urabe
 	 */
-	this.createUserListWindow = function() {
+	this.createChartSummaryWindow = function() {
 		
-		// 画面上の検索ボタンに対して押下時に実行されるイベントを登録する。
-		this.setClickEvent(SELECTOR_B_SERACH, this.clickSearchButton);
-		// 画面上の閉じるボタンに対して押下時に実行されるイベントを登録する。
-		this.setClickEvent(SELECTOR_B_CANCEL, this.closeWindow);
 		
-		// 検索ボタンを押下して初期の画面表示として、ユーザ一覧を取得する
-		$(SELECTOR_B_SERACH).click();
+		
+		
 		
 		// 画面を表示する
 		$("body").css("visibility", "visible");
@@ -40,61 +36,46 @@ function UserListWindowDetail() {
 	}
 	
 	/**
-	 * 関数名：	clickSearchButton
-	 * 概要：		検索ボタン押下時に実行されるイベント
+	 * 関数名：	clickChartViewButton
+	 * 概要：		グラフ表示ボタンを押下した際の処理
 	 * 引数：		なし
 	 * 戻り値：	なし
-	 * 作成日：	2016/12/14
+	 * 作成日：	2016/12/21
 	 * 作成者：	k.urabe
 	 */
-	this.clickSearchButton = function(selector, thisElem) {
-		
-		var jsonArray = {};			// リクエストに使用するjson連想配列を作成する
-		
-		// 検索実行前に描画エリア全体を初期状態に戻す
-		// ユーザ一覧の描画領域をすべて削除する
-		$(KEY_DIV).remove(SELECTOR_PARENT_AREA);
-		// 件数取得なしのメッセージを削除する
-		$(SELECTOR_SERACH_MESSAGE).text("");
-		// 行の見出しを削除する
-		$(KEY_DIV).remove(SELECTOR_INDEX_LINE);
-		
-		// 検索領域の各値を取得する
-		// JSON連想配列に検索対象のuser_idをセットする
-		jsonArray[KEY_USER_ID] = thisElem.getWindowItem(SELECTOR_LEFT_MENU + MARK_SPACE + SELECTOR_USER_ID);
-		// JSON連想配列に検索対象のuser_nameをセットする
-		jsonArray[KEY_USER_NAME] = thisElem.getWindowItem(SELECTOR_LEFT_MENU + MARK_SPACE + SELECTOR_USER_NAME);
-		// JSON連想配列に検索対象のminをセットする
-		jsonArray[KEY_MIN] = thisElem.getWindowItem(SELECTOR_LEFT_MENU + MARK_SPACE + SELECTOR_MIN);
-		// JSON連想配列に検索対象のmaxをセットする
-		jsonArray[KEY_MAX] = thisElem.getWindowItem(SELECTOR_LEFT_MENU + MARK_SPACE + SELECTOR_MAX);
-		// JSON連想配列に検索対象の性別をセットする
-		//jsonArray[KEY_USER_SEX] = thisElem.getWindowItem(SELECTOR_LEFT_MENU + MARK_SPACE + SELECTOR_USER_SEX);
-		jsonArray[KEY_USER_SEX] = $(SELECTOR_LEFT_MENU + MARK_SPACE + SELECTOR_USER_SEX).val();
-		
-		// JSON連想配列を用いてDBから値を取得する
-		thisElem.getJsonData(PATH_USER_MATCH_INF, jsonArray, STR_READ);
-		// 取得したJSONを用いてHTMLへタグを展開する
-		thisElem.createUserList(SELECTOR_CONTENT_AREA);
+	this.clickChartViewButton = function() {
 		
 		
 	}
 	
 	/**
-	 * 関数名：	clickUserList
-	 * 概要：		ユーザ一覧の行がクリックされた際に実行されるイベント
+	 * 関数名：	checkSearchValue
+	 * 概要：		グラフ表示ボタンを押下した際の処理
 	 * 引数：		なし
 	 * 戻り値：	なし
-	 * 作成日：	2016/12/14
+	 * 作成日：	2016/12/21
 	 * 作成者：	k.urabe
 	 */
-	this.clickUserList = function(selector, thisElem) {
-		// 別ウインドウでユーザ編集画面を開く
-		thisElem.prepareAnotherWindow(selector, thisElem, null, selector);
+	this.checkSearchValue = function() {
+		
+		
+	}
+	
+	/**
+	 * 関数名：	createChartData
+	 * 概要：		DBから取得したデータを基にグラフ用の設定値に加工する
+	 * 引数：		なし
+	 * 戻り値：	なし
+	 * 作成日：	2016/12/21
+	 * 作成者：	k.urabe
+	 */
+	this.createChartData = function() {
+		
+		
 	}
 
 }
 
-UserListWindowDetail.prototype = new WindowDesign();
+ChartSummaryWindowDetail.prototype = new WindowDesign();
 //サブクラスのコンストラクタを有効にする
-UserListWindowDetail.prototype.constructor = WindowDesign;
+ChartSummaryWindowDetail.prototype.constructor = WindowDesign;
